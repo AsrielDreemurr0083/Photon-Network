d27b1fef51b36ac5b53e14c480cb68286751cd1f
AsrielDreemurr0083 <103588221+AsrielDreemurr0083@users.noreply.github.com>
2024-10-24 15:44:19 +0900

    (2024.10.24) 대화 시스템

---
+++ Photon Network/Assets/Scripts/Characters/Character.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+[RequireComponent(typeof(Move))]
+[RequireComponent (typeof(Rotation))]
+[RequireComponent(typeof (Rigidbody))]
+public class Character : MonoBehaviourPun
+{
+    [SerializeField] Move move;
+    [SerializeField] Camera remoteCamera;
+    [SerializeField] Rotation rotation;
+    [SerializeField] Rigidbody rigidBody;
+    private void Awake()
+    {
+        move = GetComponent<Move>();
+        rotation = GetComponent<Rotation>();
+        rigidBody = GetComponent<Rigidbody>();
+    }
+    void Start()
+    {
+        DisableCamera();
+    }
+
+    private void Update()
+    {
+        if(photonView.IsMine == false)
+        {
+            return;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            MouseManager.Instance.SetMouse(true);
+        }
+
+        rotation.InputRotateY();
+    }
+
+    public void FixedUpdate()
+    {
+        if(photonView.IsMine == false)
+        {
+            return;
+        }
+
+        move.Movement(rigidBody);
+
+        rotation.RotateY(rigidBody);
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+    //    move.Movement();
+    //    rotation.RotateY();
+    //    rotation.RotateX();
+    //}
+
+    public void DisableCamera()
+    {
+        // 현재 플레이어가 나 자신이라면
+        if(photonView.IsMine) 
+        {
+            Camera.main.gameObject.SetActive(false);
+        }
+        else
+        {
+            remoteCamera.gameObject.SetActive(false);
+        }
+    }
+}
